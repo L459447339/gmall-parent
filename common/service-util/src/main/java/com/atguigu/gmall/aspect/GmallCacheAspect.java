f6884436573a243403bf9c1e3759cb72e3efb667
阿瞬 <459447339@qq.com>
2021-01-29 10:25:43 +0800

    优化缓存机制，aop切面类实现缓存，自定义注解标注的方法，方法将aop动态使用缓存功能

--- common/service-util/src/main/java/com/atguigu/gmall/aspect/GmallCacheAspect.java
+++ common/service-util/src/main/java/com/atguigu/gmall/aspect/GmallCacheAspect.java
@@ -12,7 +12,10 @@ import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
+import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
@@ -35,16 +38,23 @@ public class GmallCacheAspect {
         Class<?> returnType = methodSignature.getMethod().getReturnType();
         //获取方法名
         String methodName = methodSignature.getMethod().getName();
-        //获取注解信息
+        //获取上的注解信息
         GmallCache annotation = methodSignature.getMethod().getAnnotation(GmallCache.class);
         //拼接key
         String argKey = "";
         if(args!=null && args.length>0){
             for (Object arg : args) {
-                argKey = argKey +":"+ arg;
+                boolean flag = isUserObject(arg);
+                if(flag){
+                    argKey = argKey + ":" +arg.getClass().getTypeName();
+                }else {
+                    argKey = argKey + ":" +arg;
+                }
             }
         }
+        //获取redis存储数据的key前缀
         String key = annotation.key();
+        //获取redis存储数据类型
         String type = annotation.type();
         String currentKey = "";
         //线程获取分布式锁
@@ -53,9 +63,7 @@ public class GmallCacheAspect {
         if(lockFlag){
             if("sku".equalsIgnoreCase(key)){
                 currentKey = key + argKey + ":" + methodName;
-                if("str".equalsIgnoreCase(type)){
                     proceed = redisTemplate.opsForValue().get(currentKey);
-                }
             }
             //执行方法
             if(proceed==null){
@@ -65,12 +73,18 @@ public class GmallCacheAspect {
                     throwable.printStackTrace();
                 }
                 if(proceed!=null){
-                    redisTemplate.opsForValue().set(currentKey,proceed);
+                    if(type.toLowerCase().contains("str")){
+                        redisTemplate.opsForValue().set(currentKey,proceed);
+                    }else if(type.toLowerCase().contains("list")){
+                        redisTemplate.opsForList().leftPush(currentKey,proceed);
+                    }else if(type.toLowerCase().contains("hash")){
+                        redisTemplate.opsForHash().putAll(currentKey,(Map) proceed);
+                    }
+
                 }else {
                     redisTemplate.opsForValue().set(currentKey,null,2, TimeUnit.MINUTES);
                 }
             }
-
             //后置通知
             //使用Lua脚本让判断和释放锁同步执行
             String script = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
@@ -83,12 +97,24 @@ public class GmallCacheAspect {
             redisTemplate.execute(redisScript, Arrays.asList("lock"), lockVal);// 执行脚本
         }else {
             try {
-                TimeUnit.MINUTES.sleep(30);
+                TimeUnit.SECONDS.sleep(5);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            return cacheAroundAdvice(point);
+            proceed = redisTemplate.opsForValue().get(currentKey);
+            return proceed;
         }
         return proceed;
+    }
+
+    //判断参数是否是自定义对象类型
+    private boolean isUserObject(Object type){
+        boolean flag = false;
+        String typeName = type.getClass().getTypeName();
+        if(!typeName.toLowerCase().contains("long") && typeName.toLowerCase().contains("integer") &&
+                typeName.toLowerCase().contains("bigdecimal") && typeName.toLowerCase().contains("string")){
+            flag = true;
+        }
+        return flag;
     }
 }
